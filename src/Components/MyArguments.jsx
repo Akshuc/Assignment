24ad17e2cbdcd8a4075393a3e8f1aef005d25138
Akash Singh <94307629+Akshuc@users.noreply.github.com>
2023-03-23 17:49:20 +0530

    Add files via upload

---
+++ src/Components/MyArguments.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react'
+
+
+const MyArguments = () => {
+  const [argument, setargument] = useState([{ value: "myarg", label: "myarg", answer: "false", selected: true }]);
+  const [data, setData] = useState(new Map());
+  
+
+  useEffect(() => {
+
+    setvalue();
+  }, [argument])
+
+  var result ;
+
+
+
+  const readArg = (index) => {
+
+    const addargument = document.getElementsByClassName('myarg')[index].value;
+    const addarg = [...argument,];
+
+    const argvalue = document.getElementsByClassName('mySelect')[index].value;
+
+    addarg[index] = { value: addargument, label: addargument, answer: argvalue };
+
+    setargument(addarg);
+
+    result = addarg.answer;
+    
+    setvalue();
+
+  }
+
+  const constant = [
+    { value: "false", label: "false", answer: "false", selected: true },
+    { value: "true", label: "true", answer: "true" },
+  ];
+  const setvalue = () => {
+
+    argument.map((data, index) => {
+      setData(map => new Map(map.set(data.value, data.answer)));
+    })
+
+    constant.map((data) => {
+      setData(map => new Map(map.set(data.value, data.answer)));
+    }
+    )
+  }
+  const addArg = () => {
+    const addarg = [...argument, []];
+    setargument(addarg);
+
+
+  }
+
+
+
+
+  return (
+    <>
+     
+        <div>
+          {argument.map((data, index) => {
+            return (
+              <div key={index}>
+                <input type="text" value={data.value} name="arg" className='myarg' placeholder='newarg' onChange={(data) => readArg(index)} />
+                <select className="mySelect" onChange={() => readArg(index)}>
+                  <option value="false">False </option>
+                  <option value="true">True</option>
+                </select>
+              </div>
+            )
+          })}
+
+        </div>
+        <button id="button" onClick={addArg}>+Add Arg</button>
+
+        <br />
+
+        <h1>Result:{result}</h1>
+    </>
+  )
+}
+
+export default MyArguments;
